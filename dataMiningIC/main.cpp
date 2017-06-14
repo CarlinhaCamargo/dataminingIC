557bed9c2fea5fc9b1a95949c3ec4ad8504dcf6a
CarlinhaCamargo <carlinha-camargo@hotmail.com>
2017-06-13 23:04:36 -0300

    Populacao inicial com impressao

--- dataMiningIC/main.cpp
+++ dataMiningIC/main.cpp
@@ -10,6 +10,11 @@
 #include <stdlib.h>
 #include <time.h> 
 
+#define TAMANHO_POPULACAO 50
+#define TAMANHO_GENES     32
+
+
+
 using namespace std;
 
 //typedef enum {
@@ -27,21 +32,26 @@ typedef struct {
 }Gene;
 
 typedef struct {
-    Gene genes[32];
+    Gene genes[TAMANHO_GENES];
     int historico_familiar;
     int idade;
 }Individuo;
-
+/************ DECLARACAO DAS FUNCOES ************/
 double numeroRandomicoDouble(int inicio, int fim);
 int numeroRandomicoInt(int inicio, int fim);
-Individuo generatePopulation();
+Individuo geraIndividuo();
+void geraPopulacao(Individuo** populacao);
+void printIndividuo(Individuo individuo);
+void printPopulacao(Individuo *populacao);
 
 /*
  * 
  */
 int main(int argc, char** argv) {
     srand(time(NULL));
-    
+    Individuo *populacao;
+    geraPopulacao(&populacao);
+    printPopulacao(populacao);
     
     return 0;
 }
@@ -56,7 +66,25 @@ int numeroRandomicoInt(int inicio, int fim){
     return rand()%(fim+1-inicio)+inicio;
 }
 
-Individuo generatePopulation(){
+void printIndividuo(Individuo individuo){
+    for(int i=0; i<TAMANHO_GENES; i++){
+//        printf("Gene %d:\n", i);
+        printf("[%.2f|%d|%d]  ", individuo.genes[i].peso, individuo.genes[i].operador, individuo.genes[i].valor);
+    }
+    printf("\nHistorico: %d", individuo.historico_familiar);
+    printf("\nIdade: %d", individuo.idade);
+    printf("\n\n");
+}
+
+void printPopulacao(Individuo *populacao){
+    for(int i=0; i<TAMANHO_POPULACAO; i++){
+        printIndividuo(populacao[i]);
+    }
+    
+}
+
+
+Individuo geraIndividuo(){
     
     Individuo individuo;
     
@@ -65,6 +93,24 @@ Individuo generatePopulation(){
         individuo.genes[i].operador = numeroRandomicoInt(0,3);
         individuo.genes[i].valor = numeroRandomicoInt(0,3);
     }
+    individuo.historico_familiar = numeroRandomicoInt(0,1);
+    individuo.idade = numeroRandomicoInt(0,79);
     
     return individuo;   
 }
+/* a funcao ira modificar o conteudo de um vetor de individuos. 
+ * chamada do metodo deve seguir o formato geraPopulacao(&Individuo[]) */
+void geraPopulacao(Individuo** populacao){
+    *populacao = (Individuo *) malloc(TAMANHO_POPULACAO *(sizeof(Individuo)));
+    
+    if (*populacao == NULL){
+        return;
+    }
+    for(int i=0; i<TAMANHO_POPULACAO; i++){
+        (*populacao)[i] = geraIndividuo();
+    }
+    
+    return; 
+}
+
+
